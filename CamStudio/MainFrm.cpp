3b88e591b6cc032c676b27bd4167b40d61a111dc
romulus9 <romulus9@localhost>
2008-09-24 03:47:18 +0000

    - remove dead whitespace

--- CamStudio/MainFrm.cpp
+++ CamStudio/MainFrm.cpp
@@ -46,82 +46,69 @@ static UINT indicators[] =
 
 };
 
-
-
 /////////////////////////////////////////////////////////////////////////////
 // CMainFrame construction/destruction
 
 CMainFrame::CMainFrame()
 {
 	// TODO: add member initialization code here
-	
 }
 
 CMainFrame::~CMainFrame()
 {
-
 	//ver 1.2
 	m_ToolbarBitmap256.DeleteObject();
 	m_ToolbarBitmapMask.DeleteObject();
-
 }
-
 
 int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 {
 	if (CFrameWnd::OnCreate(lpCreateStruct) == -1)
 		return -1;
-	
-	if (!m_wndToolBar.Create(this) ||
-		!m_wndToolBar.LoadToolBar(IDR_MAINFRAME))
+
+	if (!m_wndToolBar.Create(this)
+		|| !m_wndToolBar.LoadToolBar(IDR_MAINFRAME))
 	{
 		TRACE0("Failed to create toolbar\n");
 		return -1;      // fail to create
 	}
 
-	
-
-	if (!m_wndStatusBar.Create(this) ||
-		!m_wndStatusBar.SetIndicators(indicators,
-		  sizeof(indicators)/sizeof(UINT)))
+	if (!m_wndStatusBar.Create(this)
+		|| !m_wndStatusBar.SetIndicators(indicators, sizeof(indicators)/sizeof(UINT)))
 	{
 		TRACE0("Failed to create status bar\n");
 		return -1;      // fail to create
 	}
 
-
-	
 	//ver 1.2
-	m_ToolbarBitmap256.LoadBitmap( IDB_TOOLBAR256 );    
-	m_ToolbarBitmapMask.LoadBitmap( IDB_TOOLBARMASK );    	
-		
-	HDC hScreenDC = ::GetDC(NULL);	
-	int numbits = ::GetDeviceCaps(hScreenDC, BITSPIXEL );		
-	::ReleaseDC(NULL,hScreenDC);		
-		
-	if (numbits>8) {
+	m_ToolbarBitmap256.LoadBitmap( IDB_TOOLBAR256 );
+	m_ToolbarBitmapMask.LoadBitmap( IDB_TOOLBARMASK );
 
+	HDC hScreenDC = ::GetDC(NULL);
+	int numbits = ::GetDeviceCaps(hScreenDC, BITSPIXEL );
+	::ReleaseDC(NULL,hScreenDC);
+
+	if (numbits>8) {
 		//m_ilToolBar.Create(21,21, ILC_COLOR8 | ILC_MASK, 4, 4);
 		m_ilToolBar.Create(21,21, ILC_COLOR16 | ILC_MASK, 4, 4);
 		//m_ilToolBar.Create(32,32, ILC_COLOR16 | ILC_MASK, 4, 4);
 		m_ilToolBar.Add(&m_ToolbarBitmap256,&m_ToolbarBitmapMask);
 
-		
-		//VC6 
-		//m_wndToolBar.GetToolBarCtrl().SetImageList(&m_ilToolBar);		
-		
-		//VC5
-		::SendMessage(m_wndToolBar.GetToolBarCtrl().m_hWnd, TB_SETIMAGELIST, 0, (LPARAM)m_ilToolBar.GetSafeHandle());		
+		//VC6
+		//m_wndToolBar.GetToolBarCtrl().SetImageList(&m_ilToolBar);
 
+		//VC5
+		::SendMessage(m_wndToolBar.GetToolBarCtrl().m_hWnd, TB_SETIMAGELIST, 0, (LPARAM)m_ilToolBar.GetSafeHandle());
 	}
-	
-	
+
 	m_wndToolBar.ModifyStyle(0, TBSTYLE_FLAT);
 
 	// TODO: Remove this if you don't want tool tips or a resizeable toolbar
-	m_wndToolBar.SetBarStyle(m_wndToolBar.GetBarStyle() |
-		CBRS_TOOLTIPS | CBRS_FLYBY | CBRS_SIZE_DYNAMIC);
-	
+	m_wndToolBar.SetBarStyle(m_wndToolBar.GetBarStyle()
+		| CBRS_TOOLTIPS
+		| CBRS_FLYBY
+		| CBRS_SIZE_DYNAMIC);
+
 	//m_wndToolBar.SetButtonText(1,"Record");
 	// TODO: Delete these three lines if you don't want the toolbar to
 	//  be dockable
@@ -134,19 +121,18 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	return 0;
 }
 
-
 BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
 {
 	// TODO: Modify the Window class or styles here by modifying
-	
+
 	//  the CREATESTRUCT cs
-	cs.x = 200;	
+	cs.x = 200;
 	cs.y = 200;
-	cs.cx=280;   
-	cs.cy=292;    
+	cs.cx=280;
+	cs.cy=292;
 
 	cs.style &= ~FWS_ADDTOTITLE;
-	cs.style &= ~WS_THICKFRAME; 
+	cs.style &= ~WS_THICKFRAME;
 	cs.style &= ~WS_MAXIMIZEBOX;
 
 	cs.lpszClass = _T("CamStudio");
@@ -173,23 +159,20 @@ void CMainFrame::Dump(CDumpContext& dc) const
 /////////////////////////////////////////////////////////////////////////////
 // CMainFrame message handlers
 
-void CMainFrame::OnClose() 
+void CMainFrame::OnClose()
 {
 	// TODO: Add your message handler code here and/or call default
 	if (recordstate==1) {
 
 		//MessageBox("Please stop video recording before exiting program.","Note",MB_OK | MB_ICONEXCLAMATION);
-		MessageOut(this->m_hWnd,IDS_STRING_STOPBEFOREEXIT ,IDS_STRING_NOTE,MB_OK | MB_ICONEXCLAMATION);		
-
+		MessageOut(this->m_hWnd,IDS_STRING_STOPBEFOREEXIT ,IDS_STRING_NOTE,MB_OK | MB_ICONEXCLAMATION);
 		return;
-
 	}
-	
+
 	CFrameWnd::OnClose();
 }
 
-
-void CMainFrame::OnViewCompactview() 
+void CMainFrame::OnViewCompactview()
 {
 	// TODO: Add your command handler code here
 	CRect clientrect;
@@ -204,38 +187,30 @@ void CMainFrame::OnViewCompactview()
 	int borderHeight = GetSystemMetrics(SM_CYBORDER);
 
 	//Dock Toolbar
-	DockControlBar(&m_wndToolBar);	
+	DockControlBar(&m_wndToolBar);
 
 	//Hide Menu
-	if ( hMenu != NULL )	
+	if ( hMenu != NULL )
 		::SetMenu( m_hWnd, hMenu );
 
 	//Hide Status Bar
-	CStatusBar* pStatus = (CStatusBar*) AfxGetApp()->m_pMainWnd->GetDescendantWindow(AFX_IDW_STATUS_BAR);	
+	CStatusBar* pStatus = (CStatusBar*) AfxGetApp()->m_pMainWnd->GetDescendantWindow(AFX_IDW_STATUS_BAR);
 	pStatus->ShowWindow(SW_HIDE);
 
-//	int compactcx = 280;
-//	int compactcy = 292;	
-	int compactcx = 340;
-	int compactcy = 360;
-
-	compactcx=windowrect.Width();
-	compactcy=windowrect.Height() - clientrect.Height() + toolbarrect.Height() - borderHeight;
+	int compactcx = windowrect.Width();
+	int compactcy = windowrect.Height() - clientrect.Height() + toolbarrect.Height() - borderHeight;
 	viewtype = 1;
 
-	SetWindowPos( &wndTop, windowrect.left, windowrect.top, compactcx, compactcy, SWP_SHOWWINDOW);  
-	
+	SetWindowPos( &wndTop, windowrect.left, windowrect.top, compactcx, compactcy, SWP_SHOWWINDOW);
 }
 
-void CMainFrame::OnUpdateViewCompactview(CCmdUI* pCmdUI) 
+void CMainFrame::OnUpdateViewCompactview(CCmdUI* pCmdUI)
 {
 	// TODO: Add your command update UI handler code here
 	pCmdUI->SetCheck(viewtype==1);
-	
 }
 
-
-void CMainFrame::OnViewButtonsview() 
+void CMainFrame::OnViewButtonsview()
 {
 	// TODO: Add your command handler code here
 	CRect clientrect;
@@ -246,41 +221,31 @@ void CMainFrame::OnViewButtonsview()
 	GetWindowRect( &windowrect);
 	GetClientRect( &clientrect);
 	m_wndToolBar.GetWindowRect(&toolbarrect);
-	
+
 	CStatusBar* pStatus = (CStatusBar*) AfxGetApp()->m_pMainWnd->GetDescendantWindow(AFX_IDW_STATUS_BAR);
 	pStatus->GetWindowRect(&statusbarrect);
 
 	int borderHeight = GetSystemMetrics(SM_CYBORDER);
 	int captionHeight = GetSystemMetrics(SM_CYCAPTION);
-	
+
 	//Dock Toolbar
 	DockControlBar(&m_wndToolBar);
-	
-	
+
 	//Hide menu
-	hMenu = ::GetMenu(m_hWnd);	
+	hMenu = ::GetMenu(m_hWnd);
 	::SetMenu( m_hWnd, NULL );
-	
 
 	//Hide Status Bar
 	pStatus->ShowWindow(SW_HIDE);
-	
-//	int compactcx = 280;
-//	int compactcy = 292;	
-	int compactcx = 340;
-	int compactcy = 360;
 
-	compactcx=windowrect.Width();
-	compactcy=captionHeight + toolbarrect.Height() + borderHeight + borderHeight;
-	viewtype = 2;					
-	
-	SetWindowPos( &wndTop, windowrect.left, windowrect.top, compactcx, compactcy, SWP_SHOWWINDOW);  
+	int compactcx = windowrect.Width();
+	int compactcy = captionHeight + toolbarrect.Height() + borderHeight + borderHeight;
+	viewtype = 2;
 
-
-	
+	SetWindowPos( &wndTop, windowrect.left, windowrect.top, compactcx, compactcy, SWP_SHOWWINDOW);
 }
 
-void CMainFrame::OnViewNormalview() 
+void CMainFrame::OnViewNormalview()
 {
 	// TODO: Add your command handler code here
 	CRect clientrect;
@@ -296,84 +261,58 @@ void CMainFrame::OnViewNormalview()
 	pStatus->ShowWindow(SW_SHOW);
 
 	//Dock Toolbar
-	DockControlBar(&m_wndToolBar);	
+	DockControlBar(&m_wndToolBar);
 
 	//Show Menu
-	if ( hMenu != NULL )	
+	if ( hMenu != NULL )
 		::SetMenu( m_hWnd, hMenu );
-	
-//	int compactcx = 280;
-//	int compactcy = 292;
+
 	int compactcx = 300;
 	int compactcy = 292;
 	viewtype = 0;
 
-	SetWindowPos( &wndTop, windowrect.left, windowrect.top, compactcx, compactcy, SWP_SHOWWINDOW);  
-
-	
+	SetWindowPos( &wndTop, windowrect.left, windowrect.top, compactcx, compactcy, SWP_SHOWWINDOW);
 }
 
-void CMainFrame::OnUpdateViewButtonsview(CCmdUI* pCmdUI) 
+void CMainFrame::OnUpdateViewButtonsview(CCmdUI* pCmdUI)
 {
 	// TODO: Add your command update UI handler code here
 	pCmdUI->SetCheck(viewtype==2);
-	
 }
 
-void CMainFrame::OnUpdateViewNormalview(CCmdUI* pCmdUI) 
+void CMainFrame::OnUpdateViewNormalview(CCmdUI* pCmdUI)
 {
 	// TODO: Add your command update UI handler code here
 	pCmdUI->SetCheck(viewtype==0);
-	
 }
 
-void CMainFrame::OnViewtype() 
+void CMainFrame::OnViewtype()
 {
 	// TODO: Add your command handler code here
-
-	
 	viewtype++;
 	if (viewtype>2)
-		viewtype=0;	
+		viewtype=0;
 
 	UpdateViewtype() ;
-		
-	
 }
 
-void CMainFrame::UpdateViewtype() 
+void CMainFrame::UpdateViewtype()
 {
-
-	
-	if (viewtype==0) 
+	if (viewtype==0)
 		OnViewNormalview();
-	else if (viewtype==1) 
+	else if (viewtype==1)
 		OnViewCompactview();
-	else if (viewtype==2) 
-		OnViewButtonsview();	
-
-
+	else if (viewtype==2)
+		OnViewButtonsview();
 }
-
-
 
 void CMainFrame::OnSysCommand(UINT nID, LPARAM lParam)
 {
-	
-	if(bMinimizeToTray)
-	{
-		if ((nID & 0xFFF0) == SC_MINIMIZE)
-		{
-		
-			//if( TrayShow())
-				this->ShowWindow(SW_HIDE);		
-				
+	if (bMinimizeToTray) {
+		if ((nID & 0xFFF0) == SC_MINIMIZE) {
+			ShowWindow(SW_HIDE);
+			return;
 		}
-		else
-			CFrameWnd::OnSysCommand(nID, lParam);	
 	}
-	else
-		CFrameWnd::OnSysCommand(nID, lParam);
+	CFrameWnd::OnSysCommand(nID, lParam);
 }
-
-
