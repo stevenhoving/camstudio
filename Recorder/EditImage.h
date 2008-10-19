97fb994350087ed8e53ddb49b30d13f98999f39b
romulus9 <romulus9@localhost>
2008-10-19 04:21:42 +0000

    - more control variables - some resource ID update

--- Recorder/EditImage.h
+++ Recorder/EditImage.h
@@ -2,6 +2,8 @@
 #define AFX_EDITIMAGE_H__E8731569_888C_480B_95E7_4CA727CD9207__INCLUDED_
 
 #include "TransparentWnd.h"
+#include "afxwin.h"
+#include "afxcmn.h"
 
 #if _MSC_VER > 1000
 #pragma once
@@ -16,13 +18,10 @@ class CEditImageDlg : public CDialog
 {
 // Construction
 public:
-	void UpdateGUI();
-	void PreModal(CTransparentWnd* transWnd);
 	CEditImageDlg(CWnd* pParent = NULL);   // standard constructor
 
-	CTransparentWnd* m_transWnd;
-	int m_dialogInitialized;
-	int pickingColor;
+	void UpdateGUI();
+	void PreModal(CTransparentWnd* transWnd);
 
 // Dialog Data
 	//{{AFX_DATA(CEditImageDlg)
@@ -59,8 +58,29 @@ protected:
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 private:
+	CTransparentWnd* m_transWnd;
+	int m_dialogInitialized;
+	int pickingColor;
+
 	HCURSOR m_hCursorCross;
 	HCURSOR m_hCursorArrow;
+	CButton m_ctrlButtonNoBKColor;
+	CButton m_ctrlButtonTransparent;
+	CButton m_ctrlButtonPreDefined;
+	CButton m_ctrlButtonBKColor;
+	CButton m_ctrlButtonColor;
+	CButton m_ctrlButtonPickScreenColor;
+	CStatic m_ctrlStaticColor;
+	CStatic m_ctrlStaticChoose;
+	CComboBox m_ctrlCBPredefinedShape;
+	CButton m_ctrlButtonAddBorder;
+	CStatic m_ctrlStaticBorderSize;
+	CStatic m_ctrlStaticColor2;
+	CButton m_ctrlButtonBorderColor;
+	CEdit m_ctrlEditBorderSize;
+	CSpinButtonCtrl m_ctrlSpinBorderSize;
+	CStatic m_ctrlStaticBorderGroup;
+	CButton m_ctrlButtonLoadNewImage;
 };
 
 //{{AFX_INSERT_LOCATION}}
