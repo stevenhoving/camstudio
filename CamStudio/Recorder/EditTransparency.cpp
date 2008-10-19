5371be6a2c0205489f31c3dfbea3b4a89f5f744b
romulus9 <romulus9@localhost>
2008-10-19 21:38:10 +0000

    - used dialog controls. - Made old ctor private and inaccessible. - Changed EditTransparency constructor (note comment in new constructor)

--- CamStudio/Recorder/EditTransparency.cpp
+++ CamStudio/Recorder/EditTransparency.cpp
@@ -1,6 +1,6 @@
 // EditTransparency.cpp : implementation file
 //
-
+/////////////////////////////////////////////////////////////////////////////
 #include "stdafx.h"
 #include "Recorder.h"
 #include "EditTransparency.h"
@@ -15,15 +15,36 @@ static char THIS_FILE[] = __FILE__;
 /////////////////////////////////////////////////////////////////////////////
 // CEditTransparencyDlg dialog
 
+// The standard ctor is private and inaccessible by design
 CEditTransparencyDlg::CEditTransparencyDlg(CWnd* pParent /*=NULL*/)
 : CDialog(CEditTransparencyDlg::IDD, pParent)
+, m_rbEnableTrans(m_bEnableTransOld)
+, m_bEnableTransOld(false)
+, m_riLevel(m_iLevelOld)
+, m_iLevelOld(50)
 {
 	//{{AFX_DATA_INIT(CEditTransparencyDlg)
 	// NOTE: the ClassWizard will add member initialization here
 	//}}AFX_DATA_INIT
 
-	m_backup_enableTrans = 0;
-	m_backup_valTrans = 50;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// CEditTransparencyDlg ctor
+// TODO: The ctor here is clumsy but better than it was.
+// The original version used pointers in order to provide feedback through
+// the the variables and InvalidateTransparency(). The proper way to have
+// done this would have been to pass the values in InvalidateTransparency
+// call. 
+/////////////////////////////////////////////////////////////////////////////
+CEditTransparencyDlg::CEditTransparencyDlg(int& bEnable, int& iLevel, CTransparentWnd* pParent)
+: CDialog(CEditTransparencyDlg::IDD, pParent)
+, m_rbEnableTrans(bEnable)
+, m_bEnableTransOld(bEnable)
+, m_riLevel(iLevel)
+, m_iLevelOld(iLevel)
+, m_pTransparentWnd(pParent)
+{
 }
 
 void CEditTransparencyDlg::DoDataExchange(CDataExchange* pDX)
@@ -32,6 +53,9 @@ void CEditTransparencyDlg::DoDataExchange(CDataExchange* pDX)
 	//{{AFX_DATA_MAP(CEditTransparencyDlg)
 	// NOTE: the ClassWizard will add DDX and DDV calls here
 	//}}AFX_DATA_MAP
+	DDX_Control(pDX, IDC_CHECK1, m_ctrlButtonEnableTranparency);
+	DDX_Control(pDX, IDC_TRANSSLIDER, m_ctrlSliderTransparency);
+	DDX_Control(pDX, IDC_TRANSTEXT, m_ctrlStaticTransparency);
 }
 
 BEGIN_MESSAGE_MAP(CEditTransparencyDlg, CDialog)
@@ -47,45 +71,24 @@ END_MESSAGE_MAP()
 void CEditTransparencyDlg::OnCheck1()
 {
 	// TODO: Add your control notification handler code here
-	*m_enableTrans = ((CButton *) GetDlgItem(IDC_CHECK1))->GetCheck();
+	m_rbEnableTrans = m_ctrlButtonEnableTranparency.GetCheck();
 
-	if (*m_enableTrans) {
-
-		((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->EnableWindow(TRUE);
-
-	}
-	else
-		((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->EnableWindow(FALSE);
-
-	if (m_myparent)
-		((CTransparentWnd *) m_myparent)->InvalidateTransparency();
-
+	m_ctrlSliderTransparency.EnableWindow(m_rbEnableTrans);
+	m_pTransparentWnd->InvalidateTransparency();
 }
 
 void CEditTransparencyDlg::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
 {
 	// TODO: Add your message handler code here and/or call default
-	*m_valTrans = ((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->GetPos();
+	m_riLevel = m_ctrlSliderTransparency.GetPos();
 
 	CString valstr;
-	valstr.Format("%d",*m_valTrans);
-	((CSliderCtrl *) GetDlgItem(IDC_TRANSTEXT))->SetWindowText(valstr);
+	valstr.Format("%d", m_riLevel);
+	m_ctrlStaticTransparency.SetWindowText(valstr);
 
-	if (m_myparent)
-		((CTransparentWnd *) m_myparent)->InvalidateTransparency();
+	m_pTransparentWnd->InvalidateTransparency();
 
 	CDialog::OnHScroll(nSBCode, nPos, pScrollBar);
-}
-
-void CEditTransparencyDlg::PreModal(int *enableTrans, int *valTrans, CWnd * parent)
-{
-	m_enableTrans = enableTrans;
-	m_valTrans = valTrans;
-	m_myparent = parent;
-
-	m_backup_enableTrans = *enableTrans;
-	m_backup_valTrans = *valTrans;
-
 }
 
 BOOL CEditTransparencyDlg::OnInitDialog()
@@ -93,20 +96,15 @@ BOOL CEditTransparencyDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 
 	// TODO: Add extra initialization here
-	((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->EnableWindow(TRUE);
-	((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->SetRange(0,100);
-	((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->SetPos(*m_valTrans);
+	m_ctrlSliderTransparency.EnableWindow(m_rbEnableTrans);
+	m_ctrlSliderTransparency.SetRange(0, 100);
+	m_ctrlSliderTransparency.SetPos(m_riLevel);
 
-	((CButton *) GetDlgItem(IDC_CHECK1))->SetCheck(*m_enableTrans);
-
-	if (*m_enableTrans)
-		((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->EnableWindow(TRUE);
-	else
-		((CSliderCtrl *) GetDlgItem(IDC_TRANSSLIDER))->EnableWindow(FALSE);
+	m_ctrlButtonEnableTranparency.SetCheck(m_rbEnableTrans);
 
 	CString valstr;
-	valstr.Format("%d",*m_valTrans);
-	((CSliderCtrl *) GetDlgItem(IDC_TRANSTEXT))->SetWindowText(valstr);
+	valstr.Format("%d", m_riLevel);
+	m_ctrlStaticTransparency.SetWindowText(valstr);
 
 	return TRUE; // return TRUE unless you set the focus to a control
 	// EXCEPTION: OCX Property Pages should return FALSE
@@ -114,12 +112,10 @@ BOOL CEditTransparencyDlg::OnInitDialog()
 
 void CEditTransparencyDlg::OnCancel()
 {
-	// TODO: Add extra cleanup here
-	*m_enableTrans = m_backup_enableTrans;
-	*m_valTrans = m_backup_valTrans;
+	m_rbEnableTrans = m_bEnableTransOld;
+	m_riLevel = m_iLevelOld;
 
-	if (m_myparent)
-		((CTransparentWnd *) m_myparent)->InvalidateTransparency();
+	m_pTransparentWnd->InvalidateTransparency();
 
 	CDialog::OnCancel();
 }
