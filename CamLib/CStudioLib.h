aa35af6ecf89b35806b2685a1c91becbe1c0a830
romulus9 <romulus9@localhost>
2008-10-19 15:23:37 +0000

    - add bug reports - break up CamLib into modules

--- CamLib/CStudioLib.h
+++ CamLib/CStudioLib.h
@@ -3,30 +3,19 @@
 #ifndef CSTUDIOLIB_H
 #define CSTUDIOLIB_H
 
-int GetOperatingSystem();
+#pragma once
 
-BOOL isRectEqual(RECT a, RECT b);
-void WINAPI NormalizeRect(LPRECT prc);
-void FixRectSizePos(LPRECT prc,int maxxScreen, int maxyScreen);
+#include "CamRect.h"	// generic rectangle
+#include "CamWindow.h"	// generic window
+#include "CamFile.h"	// generic file
+#include "CamSys.h"		// generic system
+#include "CamImage.h"	// generic image
 
-void SetTitleBar(CString title);
-BOOL WinYield(void);
-CString GetProgPath();
-CString FindExtension(const CString& name);
-// todo: matchSpecial unreferenced
-int matchSpecial(int keyCtrl,int keyShift,int keyAlt,int ctrlDown,int shiftDown,int altDown);
-RECT FrameWindow(HWND hWnd, int maxxScreen, int maxyScreen, RECT rcClip);
-long rounddouble(double dbl);
+// numeric
+long rounddouble(double dVal);
 
-void SaveBitmapCopy(HBITMAP& hBitmap, HDC hdc, HDC hdcbits, const RECT& rect);
-void SaveBitmapCopy(HBITMAP& hBitmap, HDC hdc, HDC hdcbits, int x, int y, int sx, int sy);
-void RestoreBitmapCopy(HBITMAP& hBitmap, HDC hdc, HDC hdcbits, const RECT& rect);
-void RestoreBitmapCopy(HBITMAP& hBitmap, HDC hdc, HDC hdcbits, int x, int y, int sx, int sy);
-HANDLE Bitmap2Dib(HBITMAP hbitmap, UINT bits);
-
-const int USE_WINDOWS_TEMP_DIR		= 0;
-const int USE_INSTALLED_DIR			= 1;
-const int USE_USER_SPECIFIED_DIR	= 2;
-CString GetTempPath(int tempPath_Access, CString specifieddir);
+// keyboard
+// TODO: matchSpecial unreferenced
+int matchSpecial(int keyCtrl, int keyShift, int keyAlt, int ctrlDown, int shiftDown, int altDown);
 
 #endif	// CSTUDIOLIB_H
