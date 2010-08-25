44e8e8011649bf11eb423b0117ec22f746ca990d
janhgm <janhgm@localhost>
2010-08-25 23:19:40 +0000

    1) Applied correct Windows Message name to communicate with MotionAlerter. MotionAlerter Windows Messages were not recognized in Camstudio. 2) Update a few files to add local manifest files to the build that should prevent side-by-side error as people get on Vista en WIN-7. (Not enough yet

--- CamStudio/Recorder/RecorderVersionReleaseInfo.h
+++ CamStudio/Recorder/RecorderVersionReleaseInfo.h
@@ -6,6 +6,8 @@
 
 // Update numbers below when new builds are published..!
 #define  CURRENT_VERSION_NUMBER  "2.6"
-#define  CURRENT_SVN_RELEASE_NUMBER  "264"
+// Add ++ to releasenumber to indicate that executable is from revision xxx or higher
+// Update versionnumber and remove ++ when official Camstudio release will be created
+#define  CURRENT_SVN_RELEASE_NUMBER  "269++"
 
 #endif // !defined(_RECORDERVERSIONRELEASEINFO__INCLUDED_)
