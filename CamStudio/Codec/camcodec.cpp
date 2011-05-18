9e19e7ed732549fd9c2d6085393e26cbf680e12e
Paul McQuade <tungstentide@gmail.com>
2011-05-18 18:25:19 +0100

    [MOVE] Codec + Commandline into main folder.

--- CamStudio/Codec/camcodec.cpp
+++ CamStudio/Codec/camcodec.cpp
@@ -96,7 +96,7 @@ Comments) 1950 to 1952 in the files ftp://ds.internic.net/rfc/rfc1950.txt
 #include "camcodec.h"
 #include "resource.h"
 #include "minilzo/minilzo.h"
-#include "../CamStudio/CxImage/zlib/zlib.h"
+#include "../CxImage/zlib/zlib.h"
 #include <stdio.h>
 
 TCHAR szDescription[] = TEXT("CamStudio Lossless Codec v1.5");
