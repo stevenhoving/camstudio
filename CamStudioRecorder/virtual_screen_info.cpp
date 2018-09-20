7ccac2b15be95704f36e9ef98787c3aa6b7677aa
Steven Hoving <stevenhoving@gmail.com>
2018-09-20 09:16:32 +0200

    add virtual screen info files.

---
+++ CamStudioRecorder/virtual_screen_info.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "virtual_screen_info.h"
+#include <windows.h>
+
+virtual_screen_info get_virtual_screen_info()
+{
+    virtual_screen_info result{};
+
+    HDC hScreenDC = ::GetDC(nullptr);
+    const auto bpp = ::GetDeviceCaps(hScreenDC, BITSPIXEL);
+    const auto max_x = ::GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    const auto max_y = ::GetSystemMetrics(SM_CYVIRTUALSCREEN);
+    const auto min_x = ::GetSystemMetrics(SM_XVIRTUALSCREEN);
+    const auto min_y = ::GetSystemMetrics(SM_YVIRTUALSCREEN);
+    const auto monitor_count = ::GetSystemMetrics(SM_CMONITORS);
+    ::ReleaseDC(nullptr, hScreenDC);
+
+    result.bpp = bpp;
+    result.size = { min_x, min_y, max_x, max_y };
+    result.monitor_count = monitor_count;
+    return result;
+}
---
+++ CamStudioRecorder/virtual_screen_info.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <CamCapture/cam_rect.h>
+
+struct virtual_screen_info
+{
+    // the virtual screen rect
+    cam::rect<int> size{ 0, 0, 0, 0 };
+    // the bits per pixel
+    int bpp{ 0 };
+    // the monitor count
+    int monitor_count{ 0 };
+};
+
+virtual_screen_info get_virtual_screen_info();
