cc9fae5bc370827412fb2301c8f6d71b3202c1de
Steven Hoving <stevenhoving@gmail.com>
2018-09-25 09:10:07 +0200

    Fix CamError functions incorrectly declared as extern.

--- CamLib/include/CamLib/CamError.h
+++ CamLib/include/CamLib/CamError.h
@@ -2,7 +2,7 @@
 
 // general error handler
 void OnError(const TCHAR *lpszFunction);
-extern void ErrorMsg(const char *frmt, ...);
-extern int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus);
-extern int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus, long val);
-extern int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus, long val1, long val2);
+void ErrorMsg(const TCHAR *frmt, ...);
+int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus);
+int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus, long val);
+int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus, long val1, long val2);
