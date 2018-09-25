a92ec7d510de792bcb8335a468bb470327dc1c47
Steven Hoving <stevenhoving@gmail.com>
2018-09-25 08:41:38 +0200

    remove CamImage from CamLib.

--- CamLib/include/CamLib/CStudioLib.h
+++ CamLib/include/CamLib/CStudioLib.h
@@ -2,7 +2,6 @@
 
 #include "CamRect.h"   // generic rectangle
 #include "CamFile.h"   // generic file
-#include "CamImage.h"  // generic image
 #include "Picture.h"
 #include "CBitmapEx.h"
 
--- CamLib/include/CamLib/CamImage.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-HANDLE Bitmap2Dib(HBITMAP hbitmap, UINT bits);
-void SaveBitmapCopy(HBITMAP &hBitmap, HDC hdc, HDC hdcbits, const RECT &rect);
-void SaveBitmapCopy(HBITMAP &hBitmap, HDC hdc, HDC hdcbits, int x, int y, int sx, int sy);
-void RestoreBitmapCopy(HBITMAP &hBitmap, HDC hdc, HDC hdcbits, const RECT &rect);
-void RestoreBitmapCopy(HBITMAP &hBitmap, HDC hdc, HDC hdcbits, int x, int y, int sx, int sy);
