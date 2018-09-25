1ab04384ddfee75fbb42658921a6114f46e7c14c
Steven Hoving <stevenhoving@gmail.com>
2018-09-25 08:47:53 +0200

    remove unused functions from CamRect in CamLib.

--- CamLib/include/CamLib/CamRect.h
+++ CamLib/include/CamLib/CamRect.h
@@ -1,6 +1,4 @@
 #pragma once
 
 // CamRect.cpp
-BOOL isRectEqual(RECT a, RECT b);
 void NormalizeRect(LPRECT prc);
-void FixRectSizePos(LPRECT prc, int maxxScreen, int maxyScreen, int minxScreen, int minyScreen);
