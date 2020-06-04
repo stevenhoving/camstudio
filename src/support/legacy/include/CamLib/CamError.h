c8b7cf20eaf14f90dbb6daf301b97266430f8682
Steven Hoving <stevenhoving@gmail.com>
2020-06-04 14:13:57 +0200

    Resolve codacy warning regarding not '\0' terminated strings.
    We resolve this by removing the function. This function has been deprecated
    for a long time.

--- src/support/legacy/include/CamLib/CamError.h
+++ src/support/legacy/include/CamLib/CamError.h
@@ -2,7 +2,6 @@
 
 // general error handler
 void OnError(const TCHAR *lpszFunction);
-void ErrorMsg(const TCHAR *frmt, ...);
 int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus);
 int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus, long val);
 int MessageOut(HWND hWnd, long strMsg, long strTitle, UINT mbstatus, long val1, long val2);
